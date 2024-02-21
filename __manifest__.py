f01e89580031bb4824dd228e0ec4e56512497efa
Yasmany Castillo <yasmany003@gmail.com>
2024-02-20 20:08:33 -0400

    New model for products brand

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Marcas de productos',
+    'category': 'Products',
+    'summary': 'Marcas de productos',
+    'version': '1.0',
+    'author': 'Yasmany Castillo',
+    'license': 'OPL-1',
+    'description': """  """,
+    'depends': ['website', 'product'],
+    'data': [
+        'views/product_brand_view.xml',
+    ],
+    'installable': True,
+}
